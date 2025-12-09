7aaf0ed4a34fdecfb1cbec2e1aa730c12ce9a6ec
Ashish Acharya <a.acharya@mobily.com.sa>
2025-12-09 10:33:26 +0530

    string programs

---
+++ practice2.py
@@ -0,0 +1,28 @@
+message= "Hey hero you are the champion of the world!"
+print(message.upper())
+print(message.lower())
+print(message.title())
+print("hhh")
+print(message.capitalize())
+print(message.replace("champion", "winner"))
+print(message.find("hero"))
+print(message.split())
+print(message.strip())
+
+print("accessing characters in a string using index")
+print(message[5])
+print(message[-2])
+print(message[2:10])
+print(message[10:])
+print(message[:5])
+
+print("Inptut and Output in python")
+
+name=input("Enter your good name: ")
+place=input ("ENter your birth place: ")
+print("Hello " + name + " from " +place+" !")
+
+greeting = (f"{name}" *5)
+print("Greeting message:", greeting)      
+
+
