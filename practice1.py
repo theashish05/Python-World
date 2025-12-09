b35790e41ce917c3d4e8d63873c0098b95990dbf
Ashish Acharya <a.acharya@mobily.com.sa>
2025-12-09 09:43:13 +0530

    My first py commit

---
+++ practice1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print ("Welcome to the world of Python programming!")
+
+
+"""Python is a high level,interpreted programming language known for its easy readability and versatility.
+It allows developers to write code in fewer lines compared to other languages like C++ or Java, making it a popular choice for 
+beginners and professionals alike. Python supports multiple programming paradigms, including procedural, object-oriented, and 
+functional programming, which enhances its flexibility in application development."""
