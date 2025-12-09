e99d7d95df5a61aa191e1e46d3f92b08c048e990
Ashish Acharya <a.acharya@mobily.com.sa>
2025-12-09 11:30:21 +0530

    Operators programs

---
+++ practice3.py
@@ -0,0 +1,60 @@
+print("Operators in python")
+
+x=10
+print(x+10)
+
+x+=3
+x-=2
+x*=2
+x/=4    
+print(x)
+
+a=100
+b=300
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+
+print("Logical Operators:")
+print((a>b) and (a<b))
+print((a>b) or (a<b))
+print(not(a>b))
+
+print("Membership Operators:")
+my_list=[1,2,3,4,5]
+my_string="Rockstar"
+
+print(3 in my_list)
+print(8 not in my_list)
+print("R" in my_string)
+print("R" not in my_string)
+
+print("Identity Operators:")
+x=[1,2,3]
+y=x 
+print(x is y)
+print(x is not y)
+y=[1,2,3]
+print(x is y)
+print(x is not y)
+
+print("Bitwise Operators:")
+a=10
+b=5
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+
+print("End of Operators in python")
+
+
+      
